0c58a786a97f7b5f469d6c404358002564c47639
yoja <yojannyreynoso@gmail.com>
2022-06-20 19:03:56 +0200

    nuevos datos

--- Proyecto juego el ahorcado/Javascript/script.js
+++ Proyecto juego el ahorcado/Javascript/script.js
@@ -1,1 +1,43 @@
 "use strict"
+
+const palabraSecreta = ["tomate", "Lechuga", "pepino", "casa", "perro", "caballo", "elefante", "dinamarca", "portugal", "cuba", "coche", "moto", "camion"]
+
+document.querySelector("#calcular").addEventListener("click", () => {
+  
+})
+
+/* console.log(palabraSecreta); */
+
+/* console.log(palabraSecreta.length); */
+
+const numeroPalabraElegida = Math.floor(Math.random()*palabraSecreta.length)
+
+const palabraElegida = palabraSecreta [numeroPalabraElegida]
+
+console.log(palabraElegida);
+
+const input = document.querySelector("input")
+
+console.log(input);
+
+const botonEmpezar = document.querySelector("button")
+
+console.log(botonEmpezar);
+
+/* const palabraElegidaLetras = palabraElegida.split("")  */
+
+const palabraConGuiones = palabraElegida.replace(/./g, " _ ");
+
+console.log(palabraConGuiones + " - " + palabraElegida);
+
+for (let i=0; i<palabraElegida.length; i++){
+
+
+}
+
+
+
+/* const palabraElegidaGuiones = palabraElegida.replace("palabraSecreta" , "_")
+console.log(palabraElegidaGuiones); */
+
+/* console.log(palabraElegida+"_"+palabraElegidaGuiones); */ 
